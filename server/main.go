f2b5bd99fbb7406f8ebc5e3d6f4615cfdca95fbb
Zackary S. <zackaria.celimane@gmail.com>
2023-09-28 21:20:34 +0100

    troubleshooting render config

--- server/main.go
+++ server/main.go
@@ -4,11 +4,20 @@ import (
 	"codetrackr/api"
 	"log"
 	"net/http"
+	"os"
 
+	"github.com/joho/godotenv"
 	"github.com/rs/cors"
 )
 
 func main() {
+	// Load environment variables from .env file
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	// Access the API key
+	PORT := os.Getenv("PORT")
 	// Set up API routes
 	router := api.SetupRoutes()
 
@@ -22,7 +31,7 @@ func main() {
 	// Use the CORS middleware with your router
 	handler := c.Handler(router)
 
-	port := ":8080"
+	port := PORT
 	log.Printf("Server is running on port %s ...\nFetching github issues opened in the last 3 months", port)
 	log.Fatal(http.ListenAndServe(port, handler))
 }
